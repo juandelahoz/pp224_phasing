6c38d406b2dbe78bad60443d0c7dc6a48faf5106
Tommer Schwarz <tommerschwarz@gmail.com>
2018-06-02 16:21:02 -0700

    multiple genotypes

--- src/segment_phasing.py
+++ src/segment_phasing.py
@@ -1,11 +1,12 @@
 import sys
 import math
 
-#gtypes = ['01210','10222','00110']
-gtypes = ['01210']
+gtypes = ['01210','10222','00110']
+#gtypes = ['01210']
 
 
 def possHaps (g):
+	pairs = []
 	num_het = 0
 	
 	for snp in g:
@@ -19,7 +20,7 @@ def possHaps (g):
 	while (i < len(g)):
 		j = 0
 		if (g[i] == '0'):
-	 		while (j < len(ph)):
+			while (j < len(ph)):
 				ph[j] = ph[j] + '0'
 				j = j + 1
 		elif (g[i] == '2'):
@@ -28,8 +29,6 @@ def possHaps (g):
 				j = j + 1
 		else:
 			switch = math.pow(2, num_het-het_passed)
-			print('het_passed: '+str(het_passed))
-			print('switch: '+str(switch))
 			on = True
 			k = 0
 			while (j < len(ph)):
@@ -47,11 +46,26 @@ def possHaps (g):
 				j = j + 1
 			het_passed = het_passed + 1
 		i = i + 1
+	for h in ph:
+		comp = ''
+		i = 0
+		while (i < len(g)):
+			if (g[i] == '0'):
+				comp = comp + '0'
+			elif (g[i] == '2'):
+				comp = comp + '1'
+			else:
+				if (h[i] == '1'):
+					comp = comp + '0'
+				else:
+					comp = comp + '1'
+			i = i + 1
+		pairs.append([h, comp])
+	return pairs
 
-	return ph
 
-
-
-
+total_haps = []
 for g in gtypes:
-	print(possHaps(g))
+	total_haps.append(possHaps(g))
+
+print(total_haps)
